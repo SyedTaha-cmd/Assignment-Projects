309ede36daa518af238705b6b4daca7c70e55d1c
SyedTaha-cmd <taha786ahmed@gmail.com>
2024-03-03 22:45:13 +0500

    assignment1

---
+++ assignment1.dart
@@ -0,0 +1,52 @@
+void main() {
+  
+  int length = 5;
+  int breadth = 5;
+  
+  int area = length * breadth;
+  
+  print("Q1");
+  print("result: $area");
+  print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+
+int num = 7;
+  int add = num + 8;
+  
+  double div = add / 3;
+  
+  double per = div % 5;
+  
+  double mult = per * 5;
+  print("Q2");
+  print("result: $mult");
+   print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+
+   int a = 52;
+  int b = 78;
+  
+  bool bothcondition = a < 50 && a < b;
+  bool onecondition = a < 50 || a < b;
+    
+    print("Q3");
+ print("Both-Condition: $bothcondition");
+  print("One-Condition: $onecondition");
+   print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+
+    int english = 78;
+  int urdu = 45;
+  int math = 62;
+  
+  int total = 300;
+  int obt = english + urdu + math;
+  
+  var percent = obt/total*100;
+  
+  print("Q4");
+  print("BILAL");
+  print("English: $english");
+  print("Urdu: $urdu");
+  print("Math: $math");
+  print("Total Marks: $obt / $total");
+  print("Percentage: $percent%");
+  print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+}
